737e7b35bfe7652fb997f941c04b41db9c2f70d5
Pete Nelson <petenelson@fortress.local>
2015-12-10 11:10:06 -0600

    more work

--- includes/class-advanced-permalinks-examples-admin.php
+++ includes/class-advanced-permalinks-examples-admin.php
@@ -4,16 +4,175 @@ if ( ! class_exists( 'Advanced_Permalinks_Examples_Post_Meta' ) ) {
 
 	class Advanced_Permalinks_Examples_Admin {
 
+		/**
+		 * Initialize the admin portion of the plugin
+		 * @return void
+		 */
 		public function admin_init() {
 
-			/*
-			TODO
-				show cpt, metabox for About page
-				person cpt, metabox for Bio page
-			*/
+			$this->add_meta_boxes();
 
+			// custom columns in season
+			add_filter('manage_edit-season_columns',         array( $this, 'add_custom_season_columns' ) );
+			add_action('manage_season_posts_custom_column',  array( $this, 'render_custom_season_column'), 10, 2 );
 
 		}
+
+		private function add_meta_boxes() {
+
+			// add a metabox to the show CPT to select the About page
+			add_meta_box(
+				'show-about',
+				'About Page',
+				array( $this, 'about_page_metabox' ),
+				'show',
+				'side'
+				);
+
+			// save the show's About page
+			add_action( 'save_post_show', array( $this, 'save_about_page_metabox' ), 10, 2 );
+
+			// add a metabox to allow associating items to a show
+			add_meta_box(
+				'post-for-show',
+				'Show',
+				array( $this, 'post_for_show_metabox' ),
+				array( 'post', 'season' ),
+				'side'
+				);
+
+			// save the post's Show parent
+			add_action( 'save_post', array( $this, 'save_post_for_show_metabox' ), 10, 2 );
+
+		}
+
+
+		public function about_page_metabox( $post ) {
+
+			wp_nonce_field( 'show-about-page', 'show-about-page-nonce' );
+
+			$about_page_id = get_post_meta( $post->ID, '_about_page_id', true );
+			$page_query = new WP_Query( array(
+				'post_type'       => 'page',
+				'posts_per_page'  => 500,
+				'orderby'         => 'name',
+				'order'           => 'ASC',
+				)
+			);
+
+			?>
+				<select name="_about_page_id">
+					<option value="">-Select-</option>
+					<?php foreach( $page_query->posts as $page ) : ?>
+						<option value="<?php echo esc_attr( $page->ID ); ?>" <?php selected( $page->ID, $about_page_id ); ?>><?php echo esc_html( $page->post_title ); ?></option>
+					<?php endforeach; ?>
+				</select>
+			<?php
+		}
+
+
+		public function save_about_page_metabox( $post_id, $post ) {
+			if ( ! wp_verify_nonce( filter_input( INPUT_POST, 'show-about-page-nonce', FILTER_SANITIZE_STRING ), 'show-about-page' ) ) {
+				return;
+			}
+
+			$about_page_id = filter_input( INPUT_POST, '_about_page_id', FILTER_SANITIZE_NUMBER_INT );
+			if ( ! empty( $about_page_id ) ) {
+				update_post_meta( $post_id, '_about_page_id', $about_page_id );
+			} else {
+				delete_post_meta( $post_id, '_about_page_id' );
+			}
+		}
+
+		public function post_for_show_metabox( $post ) {
+
+			wp_nonce_field( 'post-for-show', 'post-for-show-nonce' );
+
+			$show_query = new WP_Query( array(
+				'post_type'       => 'show',
+				'posts_per_page'  => 500,
+				'orderby'         => 'name',
+				'order'           => 'ASC',
+				)
+			);
+
+			?>
+				<select name="_post_for_show_id">
+					<option value="">-Select-</option>
+					<?php foreach( $show_query->posts as $show ) : ?>
+						<option value="<?php echo esc_attr( $show->ID ); ?>" <?php selected( $show->ID, $post->post_parent ); ?>><?php echo esc_html( $show->post_title ); ?></option>
+					<?php endforeach; ?>
+				</select>
+			<?php
+		}
+
+
+		/**
+		 * Associates a post with a show
+		 */
+		public function save_post_for_show_metabox( $post_id, $post ) {
+			if ( ! in_array( $post->post_type, array( 'post', 'season' ) )  || ! wp_verify_nonce( filter_input( INPUT_POST, 'post-for-show-nonce', FILTER_SANITIZE_STRING ), 'post-for-show' ) ) {
+				return;
+			}
+
+			$post_parent = filter_input( INPUT_POST, '_post_for_show_id', FILTER_SANITIZE_NUMBER_INT );
+			if ( ! empty( $post_parent ) ) {
+				$post->post_parent = $post_parent;
+
+				// unhook this function temporarily so it isn't called again by the wp_update_post call
+				remove_action( 'save_post', array( $this, 'save_post_for_show_metabox' ), 10, 2 );
+
+				// save the updated post
+				wp_update_post( $post );
+
+				// reenable hook
+				add_action( 'save_post', array( $this, 'save_post_for_show_metabox' ), 10, 2 );
+
+			}
+
+		}
+
+
+		function add_custom_season_columns( $columns ) {
+
+			$new_columns = array(
+				'show' => 'Show',
+				'slug' => 'Slug',
+			);
+
+			$offset = 2;
+			$columns = array_slice( $columns, 0, $offset, true ) +
+            	$new_columns +
+            	array_slice( $columns, $offset, NULL, true );
+
+			return $columns;
+
+		}
+
+
+		function render_custom_season_column( $name, $id ) {
+
+			$value = '';
+
+			switch ( $name ) {
+				case 'slug':
+					$value = get_post_field( 'post_name', $id );
+					break;
+				case 'show':
+					$parent = get_post_field( 'post_parent', $id );
+					if ( ! empty( $parent ) ) {
+						$value = get_post_field( 'post_title', $parent );
+					}
+					break;
+
+			}
+
+			if ( ! empty( $value ) ) {
+				echo esc_html( $value );
+			}
+
+		}
+
 
 	}
 
--- includes/class-advanced-permalinks-examples.php
+++ includes/class-advanced-permalinks-examples.php
@@ -4,46 +4,343 @@ if ( ! class_exists( 'Advanced_Permalinks_Examples' ) ) {
 
 	class Advanced_Permalinks_Examples {
 
+		const VERSION = '1.0.0';
+
+		/**
+		 * Initialize our example code
+		 *
+		 * @return void
+		 */
 		public function init() {
 
+			// custom post types for the example code
 			$this->register_custom_post_types();
 
-			$this->rewrite_rules();
+			// custom rewrite rules
+			$this->add_rewrite_rules();
 
-			add_filter( 'post_type_link', array( $this, 'create_permalink' ), 10, 2 );
+			// filter for creating permalinks
+			add_filter( 'pre_post_link',         array( $this, 'pre_post_permalink' ), 10, 2 ); // posts
+			add_filter( 'page_link',             array( $this, 'page_permalink' ), 10, 2 ); // pages
+			add_filter( 'post_type_link',        array( $this, 'custom_post_type_permalink' ), 10, 2 ); // custom post types
 
-			add_action( 'pre_get_posts', array( $this, 'query_handler' ) );
+			// filter for our custom URL tags, called after the pre_post_link runs
+			add_filter( 'post_link',             array( $this, 'replace_custom_url_tags' ), 10, 2 );
 
+			// filter for custom queries based on rewrite rules
+			// note that this is an action, not a filter
+			add_action( 'pre_get_posts',         array( $this, 'query_handler' ) );
 
+			// filter for additional query vars
+			add_filter( 'query_vars',            array( $this, 'add_custom_query_vars' ) );
+
+			// lets us assign the same post slugs to season
+			add_filter( 'wp_unique_post_slug',   array( $this, 'allow_duplicate_season_slugs' ), 20, 6 );
+
+			// an admin-ajax endpoint is handy for testing permalink generation
+			add_action( 'wp_ajax_generate-permalink',        array( $this, 'ajax_generate_permalink' ) );
+			add_action( 'wp_ajax_nopriv_generate-permalink', array( $this, 'ajax_generate_permalink' ) );
 
 		}
 
-		/**
-		 * Filter for creating custom permalinks
-		 * @param  string $url  original URL
-		 * @param  object $post the WP_Post object
-		 * @return string       updated URL
-		 */
-		function create_permalink( $url, $post ) {
 
-			// https://codex.wordpress.org/Plugin_API/Filter_Reference/post_type_link
+		/**
+		 * Filter for creating custom permalinks for 'post' post types
+		 * https://codex.wordpress.org/Plugin_API/Filter_Reference/post_link
+		 *
+		 * @param  string   $url   original URL
+		 * @param  object   $post  the WP_Post object
+		 * @return string          updated URL
+		 */
+		function pre_post_permalink( $url, $post ) {
+
+			// create a new URL this post is associated with a show
+			if ( ! empty( $post->post_parent ) ) {
+
+				$parent = get_post( $post->post_parent );
+				if ( ! empty( $parent ) && 'show' === $parent->post_type ) {
+					// WordPress will automatically add the site URL after this filter is called,
+					// so we only return the path. It will also run the post_link filter after this
+					// which we've hooked into for our custom tags
+					$url = "/shows/{$parent->post_name}/blog/%year%/%monthnamefull%/%postname%";
+				}
+
+			}
+
 
 			return $url;
 		}
 
 
-		function rewrite_rules() {
+		/**
+		 * Filter for creating custom permalinks for 'page' post types
+		 * https://codex.wordpress.org/Plugin_API/Filter_Reference/page_link
+		 */
+		function page_permalink( $url, $page ) {
+
+			// creates a URL if this page is assigned as the About page for a show
+			// url: /shows/game-of-thrones/about
+			// rewrite rule: '^shows/([^/]+)/about/?$', 'index.php?show=$matches[1]&_subpage=1'
+
+			// run a post meta query to see if this is asigned to a show
+			$show_page = $this->get_show_by_about_page( $page->ID );
+			if ( ! empty( $show_page ) ) {
+				$url = home_url( "/shows/{$show_page->post_name}/about" );
+			}
+
+			return $url;
+
+		}
 
 
+		/**
+		 * Filter for creating custom permalinks for 'page' post types
+		 * https://codex.wordpress.org/Plugin_API/Filter_Reference/post_type_link
+		 */
+		function custom_post_type_permalink( $url, $custom_post_type ) {
 
+			if ( 'show' === $custom_post_type->post_type ) {
+				$url = home_url( "/shows/{$custom_post_type->post_name}" );
+			}
+
+			if ( 'season' === $custom_post_type->post_type && ! empty( $custom_post_type->post_parent ) ) {
+
+				$parent = get_post( $custom_post_type->post_parent );
+				if ( ! empty( $parent ) && 'show' === $parent->post_type ) {
+					$url = home_url( "/shows/{$parent->post_name}/{$custom_post_type->post_name}" );
+				}
+
+			}
+
+			return $url;
+
+		}
+
+
+		function add_rewrite_rules() {
+
+			// show
+			// ex: /shows/game-of-thrones
+			add_rewrite_rule( '^shows/([^/]+)/?$', 'index.php?show=$matches[1]', 'top' );
+
+			// a show's about page
+			// ex: /shows/game-of-thrones/about
+			// WordPress 4.4 has the ability to take an array of params instead of a querystring
+			add_rewrite_rule( '^shows/([^/]+)/about/?$', array(
+				'show'       => '$matches[1]',
+				'_subpage'   => 'about'
+				),
+				'top'
+			);
+
+			// a season for a show
+			// ex: /shows/game-of-thrones/season-1
+			// ex: /shows/game-of-thrones/season-two
+			add_rewrite_rule( '^shows/([^/]+)/([^/]+)/?$', array(
+				'show'       => '$matches[1]',
+				'season'     => '$matches[2]',
+				'_subpage'   => 'season'
+				),
+				'top'
+			);
+
+
+			// only flush the rewrite rules if the version number has changed
+			if ( Advanced_Permalinks_Examples::VERSION !== get_option( 'Advanced_Permalinks_Examples_Version' ) ) {
+				flush_rewrite_rules();
+				update_option( 'Advanced_Permalinks_Examples_Version', Advanced_Permalinks_Examples::VERSION );
+			}
+
+		}
+
+
+		function add_custom_query_vars( $vars ) {
+			// you can also use add_rewrite_tag, but this is a bit easier
+			$vars[] = '_subpage';
+			return $vars;
 		}
 
 
 		public function query_handler( $query ) {
 
+			// important, don't make any changes unless this is the main, frontend query
+			if ( is_admin() || ! $query->is_main_query() ) {
+				return;
+			}
+
+			if ( '1' === filter_input( INPUT_GET, 'query-debug', FILTER_SANITIZE_STRING ) ) {
+				// lets us debug the query before we modify it
+				wp_send_json( $query );
+			}
+
+
+			$show_slug   = $query->get( 'show' );
+			$subpage     = $query->get( '_subpage' );
+
+			// update the query for show's About page
+			if ( ! empty( $show_slug ) && 'about' === $subpage ) {
+
+				// find the show page
+				$show_page = $this->get_show_by_slug( $show_slug );
+
+
+				if ( ! empty( $show_page ) ) {
+
+					// if the show's About page is set, reset the query to that page
+					$about_page_id = get_post_meta( $show_page->ID, '_about_page_id', true );
+					if ( ! empty( $about_page_id  ) ) {
+
+						$query->parse_query(
+							array(
+								'post_type' => 'page',
+								'p' => $about_page_id
+								)
+							);
+
+					}
+
+				}
+
+			}
+
+
+			// update the query for a season attached to a show
+			if ( ! empty( $show_slug ) && 'season' === $subpage ) {
+
+				// find the show page
+				$show_page = $this->get_show_by_slug( $show_slug );
+
+				if ( ! empty( $show_page ) ) {
+
+					// no need to reset the query, just add the show as the post parent
+					$query->set( 'post_parent', $show_page->ID );
+
+				}
+
+			}
+
+
+			if ( '2' === filter_input( INPUT_GET, 'query-debug', FILTER_SANITIZE_STRING ) ) {
+				// lets us debug the query after we modify it
+				wp_send_json( $query );
+			}
 		}
 
 
+		function replace_custom_url_tags( $url, $post ) {
+
+			// this is based on the code in core's link-template.php
+
+			// a list of tags we'll search for in the URL
+			$tags = array(
+				'%monthnameshort%',
+				'%monthnamefull%',
+				);
+
+			// add the custom date fields
+			$fields = explode( " ", date( 'M F', strtotime( $post->post_date ) ) );
+
+			// convert everything to lowerecase
+			$fields = array_map( 'strtolower', $fields );
+
+			// return the URL with tags replaced by data from the post
+			return str_replace( $tags, $fields, $url );
+
+		}
+
+
+		/**
+		 * Allows duplicate post slugs for the season custom post type between different post parents
+		 *
+		 * @param string $slug          The post slug.
+		 * @param int    $post_ID       Post ID.
+		 * @param string $post_status   The post status.
+		 * @param string $post_type     Post type.
+		 * @param int    $post_parent   Post parent ID
+		 * @param string $original_slug The original post slug.
+		 */
+		function allow_duplicate_season_slugs( $slug, $post_id, $post_status, $post_type, $post_parent, $original_slug ) {
+
+			global $wpdb;
+
+			if ( 'season' !== $post_type || 'publish' !== $post_status ) {
+				return $slug;
+			}
+
+			$check_sql       = "
+				SELECT post_name
+				FROM $wpdb->posts
+				WHERE post_name = %s
+				AND post_type = %s
+				AND ID != %d
+				AND post_parent = %d
+				LIMIT 1
+			";
+
+			// if we're adding a new season
+			if ( empty( $post_parent ) && ! empty( $_POST['_post_for_show_id'] ) ) {
+				$post_parent = absint( $_POST['_post_for_show_id'] );
+			}
+
+			$post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $original_slug, $post_type, $post_id, $post_parent ) );
+			if ( ! empty( $post_name_check ) ) {
+
+				$suffix = 2;
+				do {
+					$alt_post_name   = substr( $original_slug, 0, 200 - ( strlen( $suffix ) + 1 ) ) . "-$suffix";
+					$post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $alt_post_name, $post_type, $post_id, $post_parent ) );
+					$suffix ++;
+				} while ( ! empty( $post_name_check ) );
+				$slug = $alt_post_name;
+
+			} else {
+				$slug = $original_slug;
+			}
+
+			return $slug;
+
+		}
+
+
+
+		function get_show_by_slug( $show_slug ) {
+
+			$query = new WP_Query( array(
+				'post_type'   => 'show',
+				'name'        => $show_slug,
+				)
+			);
+
+			if ( $query->have_posts() ) {
+				return $query->posts[0];
+			} else {
+				return null;
+			}
+		}
+
+
+		function get_show_by_about_page( $page_id ) {
+
+			$query = new WP_Query( array(
+				'post_type'    => 'show',
+				'meta_key'     => '_about_page_id',
+				'meta_value'   => $page_id,
+				)
+			);
+
+			if ( $query->have_posts() ) {
+				return $query->posts[0];
+			} else {
+				return null;
+			}
+		}
+
+
+		/**
+		 * Registers custom post types for our example (show, season, episode, person)
+		 *
+		 * @return void
+		 */
 		function register_custom_post_types() {
 
 			$args = array(
@@ -52,6 +349,10 @@ if ( ! class_exists( 'Advanced_Permalinks_Examples' ) ) {
 					'name' => 'Shows',
 					'singular_name' => 'Show'
 					),
+				'rewrite' => array(
+					'slug' => 'shows'
+					),
+				'rewrite' => false, // we will create custom rewrites for this
 				);
 
 			register_post_type( 'show', $args );
@@ -63,6 +364,7 @@ if ( ! class_exists( 'Advanced_Permalinks_Examples' ) ) {
 					'name' => 'Seasons',
 					'singular_name' => 'Season'
 					),
+				'rewrite' => false, // we will create custom rewrites for this
 				);
 
 			register_post_type( 'season', $args );
@@ -70,24 +372,19 @@ if ( ! class_exists( 'Advanced_Permalinks_Examples' ) ) {
 			$args = array(
 				'public' => true,
 				'labels' => array(
-					'name' => 'Episodes',
-					'singular_name' => 'Episode'
-					),
-				);
-
-			register_post_type( 'episode', $args );
-
-
-			$args = array(
-				'public' => true,
-				'labels' => array(
 					'name' => 'People',
 					'singular_name' => 'Person'
 					),
+				'rewrite' => false, // we will create custom rewrites for this
 				);
 
 			register_post_type( 'person', $args );
 
+		}
+
+
+		function ajax_generate_permalink() {
+			wp_send_json( get_permalink( filter_input( INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT ) ) );
 		}
 
 
